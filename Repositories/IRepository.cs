6c8aa37da2df656a40306ea078bdd45349905f58
Aleksey Radevich <virtuain98@gmail.com>
2017-08-10 13:45:05 +0300

    Generic repository with CRUD maintaining was created.

---
+++ Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EF6CodeFirstApplication.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        void Create(TEntity entityItem);
+        IEnumerable<TEntity> Read();
+        IEnumerable<TEntity> Read(Func<TEntity, bool> predicate);
+        void Remove(TEntity entityItem);
+        void Update(TEntity entityItem);
+    }
+}
---
+++ Repositories/Repository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+
+namespace EF6CodeFirstApplication.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext Context;
+        protected readonly DbSet<TEntity> SetOfEntities;
+
+        public Repository(DbContext context)
+        {
+            Context = context;
+            SetOfEntities = Context.Set<TEntity>();
+        }
+
+        public void Create(TEntity entityItem)
+        {
+            SetOfEntities.Add(entityItem);
+            Context.SaveChanges();
+        }
+
+        public IEnumerable<TEntity> Read()
+        {
+            return SetOfEntities.AsNoTracking().ToList();
+        }
+
+        public IEnumerable<TEntity> Read(Func<TEntity, bool> predicate)
+        {
+            return Context.Set<TEntity>().Where(predicate);
+        }
+
+        public void Remove(TEntity item)
+        {
+            SetOfEntities.Remove(item);
+            Context.SaveChanges();
+        }
+
+        public void Update(TEntity item)
+        {
+            Context.Entry(item).State = EntityState.Modified;
+            Context.SaveChanges();
+        }
+    }
+}
